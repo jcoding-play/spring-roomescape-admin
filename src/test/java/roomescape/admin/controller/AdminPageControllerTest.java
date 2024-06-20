80a132319b8dbbeddbffde60a4e9e7b2191b5445
gyungchan Jo <namejgc@naver.com>
2024-06-20 14:27:03 +0900

    refactor: 테스트 중복 코드 분리

--- src/test/java/roomescape/admin/controller/AdminPageControllerTest.java
+++ src/test/java/roomescape/admin/controller/AdminPageControllerTest.java
@@ -1,24 +1,11 @@
 package roomescape.admin.controller;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+import roomescape.AcceptanceTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-class AdminPageControllerTest {
-
-    @LocalServerPort
-    int port;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
+class AdminPageControllerTest extends AcceptanceTest {
 
     @Test
     @DisplayName("어드민 메인 페이지를 응답한다.")
--- src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
@@ -1,25 +1,12 @@
 package roomescape.reservation.controller;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+import roomescape.AcceptanceTest;
 
 import static org.hamcrest.Matchers.is;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-class ReservationControllerTest {
-
-    @LocalServerPort
-    int port;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
+class ReservationControllerTest extends AcceptanceTest {
 
     @Test
     void findAllReservations() {
