38a5381a7bec83e6f5113be1e719fd36cf7c2ff9
gyungchan Jo <namejgc@naver.com>
2024-06-20 23:55:51 +0900

    refactor: 예약 추가 기능 수정

--- src/main/java/roomescape/reservation/repository/JdbcReservationRepository.java
+++ src/main/java/roomescape/reservation/repository/JdbcReservationRepository.java
@@ -1,14 +1,12 @@
 package roomescape.reservation.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.ReservationRepository;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public class JdbcReservationRepository implements ReservationRepository {
@@ -24,17 +22,10 @@ public class JdbcReservationRepository implements ReservationRepository {
 
     @Override
     public Reservation save(Reservation reservation) {
-        String name = reservation.getName();
-        LocalDate date = reservation.getDate();
-        LocalTime time = reservation.getTime();
+        SqlParameterSource params = new BeanPropertySqlParameterSource(reservation);
+        long id = jdbcInsert.executeAndReturnKey(params).longValue();
 
-        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
-                .addValue("name", name)
-                .addValue("date", date)
-                .addValue("time", time);
-
-        long id = jdbcInsert.executeAndReturnKey(sqlParameterSource).longValue();
-        return new Reservation(id, name, date, time);
+        return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
     }
 
     @Override
